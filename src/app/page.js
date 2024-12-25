b50c233f93d5a181aef54815af387bad45f9407e
FurriesGang <furries@duck.com>
2024-12-26 04:11:23 +0700

    mobile fix

--- src/app/page.js
+++ src/app/page.js
@@ -3,34 +3,33 @@ import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-10 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-        <div className="flex justify-center gap-4 w-full max-w-4xl">
-          <div className="backdrop-blur-sm flex flex-col items-center justify-center text-center bg-gray-800/60 text-white p-6 rounded-lg shadow-md w-full max-w-md">
-            <div className="flex items-center space-x-4 justify-center">
-              <div className="p-2 rounded text-center items-center">
-                <div className="mb-4 flex items-center justify-center">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-10 gap-6 sm:p-10 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-6 row-start-2 items-center w-full">
+        <div className="flex flex-col lg:flex-row justify-center gap-4 w-full max-w-4xl">
+          {/* Profile Card */}
+          <div className="backdrop-blur-sm flex flex-col items-center justify-center text-center bg-gray-800/60 text-white p-4 sm:p-6 rounded-lg shadow-md w-full">
+            <div className="flex flex-col items-center space-y-4 justify-center w-full">
+              <div className="p-2 rounded text-center items-center w-full">
+                <div className="mb-4 flex flex-col sm:flex-row items-center justify-center">
                   <Image
-                    className="w-24 h-24 rounded-full shadow-lg border border-solid border-black/[.08] dark:border-white/[.145] mr-4"
+                    className="w-20 h-20 sm:w-24 sm:h-24 rounded-full shadow-lg border border-solid border-black/[.08] dark:border-white/[.145] mb-3 sm:mb-0 sm:mr-4"
                     aria-hidden
                     src="https://cdn.discordapp.com/avatars/1220230816081248289/1c11d181ef6fd8b5c20d1911c8ff2f8a?size=1024"
                     alt="My Discord PFP"
                     width={128}
                     height={128}
                   />
-                  <div className="flex flex-col">
+                  <div className="flex flex-col items-center sm:items-start">
                     <p className="text-lg font-medium mb-1">FurryGang</p>
-                    <span
-                      style={{ marginLeft: "1.5rem" }}
-                      className="bg-purple-300 text-slate-700 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full"
-                    >
+                    <span className="bg-purple-300 text-slate-700 text-xs font-medium px-2.5 py-0.5 rounded-full">
                       she/her/they 🏳️‍⚧️
                     </span>
                   </div>
                 </div>
+
                 <div className="text-center items-center">
                   <div className="text-left items-start">
-                    <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+                    <span className="bg-indigo-100 text-indigo-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
                       About Me:
                     </span>
                     <p className="mb-3"></p>
@@ -50,7 +49,7 @@ export default function Home() {
                     </p>
                     <p className="mb-4"></p>
                     <div className="text-left items-start">
-                      <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+                      <span className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
                         Organizations:
                       </span>
                       <p className="mb-3"></p>
@@ -59,7 +58,7 @@ export default function Home() {
                       I work with a few Organizations, such as:
                     </p>
                     <p className="text-sm">
-                      <a href="https://github.com/osu-somtum" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">osu-somtum</a>, <a href="https://github.com/notmycode-labs" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">notmycode-labs</a>, <a href="https://github.com/osu-somtum" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">osu-somtum</a>, and <a href="https://github.com/Minehut-Improvements" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Minehut-Improvements</a>!
+                      <a href="https://github.com/osu-somtum" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">osu-somtum</a>, <a href="https://github.com/notmycode-labs" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">notmycode-labs</a>, <a href="https://github.com/osu-somtum" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">osu-somtum</a>, and <a href="https://github.com/Minehut-Improvements" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Minehut-Improvements</a>!
                     </p>
                     <p className="mb-4"></p>
                   </div>
@@ -68,9 +67,9 @@ export default function Home() {
                       You can contact me on these platforms!
                     </p>
                   </div>
-                  <div className="mb-4 text-center flex gap-4 items-center justify-center flex-col sm:flex-row">
+                  <div className="mb-4 text-center flex gap-3 items-center justify-center flex-col sm:flex-row">
                     <a
-                      className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center px-3 py-2 text-sm font-medium text-center text-white gap-2"
+                      className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-6 items-center text-sm font-medium text-center text-white gap-2 w-full sm:w-auto"
                       href="mailto:epicmac2010@notmycode.dev"
                       rel="noopener noreferrer"
                     >
@@ -83,9 +82,8 @@ export default function Home() {
                       Email
                     </a>
                     <a
-                      className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center px-3 py-2 text-sm font-medium text-center text-white gap-2"
+                      className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-6 items-center text-sm font-medium text-center text-white gap-2 w-full sm:w-auto"
                       href="https://discord.com/users/1220230816081248289"
-                      
                       rel="noopener noreferrer"
                     >
                       <Image
@@ -105,13 +103,14 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="backdrop-blur-sm bg-gray-800/60 text-white p-6 rounded-lg shadow-md w-full max-w-md justify-center">
+          {/* Social Media Card */}
+          <div className="backdrop-blur-sm bg-gray-800/60 text-white p-4 sm:p-6 rounded-lg shadow-md w-full">
             <div className="flex flex-col justify-center items-center gap-4 text-center">
-            <span class="bg-purple-100 text-purple-800 text-1xl font-medium me-2 px-2.5 py-0.5 rounded-full">My Social Media</span>
-              <p className="mb-4 text-sm">My Online Accounts! Get in touch and connect with me!</p>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <span className="bg-purple-100 text-purple-800 text-lg font-medium px-2.5 py-0.5 rounded-full">My Social Media</span>
+              <p className="mb-2 text-sm">My Online Accounts! Get in touch and connect with me!</p>
+              <div className="grid grid-cols-2 gap-3 w-full">
                 <a
-                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center justify-center gap-2"
+                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-4 items-center justify-center gap-2"
                   href="https://discord.com/users/1220230816081248289"
                   rel="noopener noreferrer"
                 >
@@ -119,7 +118,7 @@ export default function Home() {
                   Discord
                 </a>
                 <a
-                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center justify-center gap-2"
+                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-4 items-center justify-center gap-2"
                   href="https://t.me/furriesgang"
                   rel="noopener noreferrer"
                 >
@@ -127,7 +126,7 @@ export default function Home() {
                   Telegram
                 </a>
                 <a
-                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center justify-center gap-2"
+                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-4 items-center justify-center gap-2"
                   href="https://github.com/UnitieG"
                   rel="noopener noreferrer"
                 >
@@ -135,12 +134,13 @@ export default function Home() {
                     className="invert"
                     src="/github-mark.svg"
                     alt="Github icon"
-                    width={16} height={16}
+                    width={16}
+                    height={16}
                   />
                   Github
                 </a>
                 <a
-                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 sm:h-12 px-4 sm:px-10 items-center justify-center gap-2"
+                  className="no-underline bg-gray-800/60 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-black hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent inline-flex h-10 px-4 items-center justify-center gap-2"
                   href="https://steamcommunity.com/id/unityglol/"
                   rel="noopener noreferrer"
                 >
@@ -148,9 +148,15 @@ export default function Home() {
                   Steam
                 </a>
               </div>
-              <span class="bg-yellow-100 text-yellow-800 text-1xl font-medium me-2 px-2.5 py-0.5 rounded-full">Funny Screenshot</span>
-              <div>
-                <Image className="rounded" src="/bruh.png" alt="bruh" width={384} height={216} />
+              <span className="bg-yellow-100 text-yellow-800 text-lg font-medium px-2.5 py-0.5 rounded-full">Funny Screenshot</span>
+              <div className="w-full">
+                <Image 
+                  className="rounded w-full h-auto" 
+                  src="/bruh.png" 
+                  alt="bruh" 
+                  width={384} 
+                  height={216}
+                />
               </div>
             </div>
           </div>

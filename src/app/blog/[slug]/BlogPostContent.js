c5d7ec42cda0dc43576ee6b3533c01caac64121c
unitieg <furries@duck.com>
2025-03-04 12:58:37 +0700

    fixing footer

--- src/app/blog/[slug]/BlogPostContent.js
+++ src/app/blog/[slug]/BlogPostContent.js
@@ -36,8 +36,8 @@ export default function BlogPostContent({ post }) {
             </article>
           </div>
         </div>
+        <Footer/>
       </main>
-      <Footer/>
     </div>
   );
 }
